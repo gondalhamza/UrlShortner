9fc988687db88da58fdcd9f62cec8acc5caad92e
Hamza Zubair <gondal.humza@gmail.com>
2021-10-26 02:52:53 +0500

    Cache implemented

--- app/controllers/url_compressor_controller.rb
+++ app/controllers/url_compressor_controller.rb
@@ -9,6 +9,7 @@ class UrlCompressorController < ApplicationController
       url_array = []
       params[:url].each do |url|
       short_url = Shortener::ShortenedUrl.generate(url)
+      Rails.cache.write(short_url.unique_key, url, expires_in: 43200.minutes)
       short_url.save
       puts "#{request.inspect}"
       url_array <<  request.base_url + "/" + short_url.unique_key
@@ -19,13 +20,22 @@ class UrlCompressorController < ApplicationController
 
   end
   def redirect_to_base_url
+    url = Rails.cache.read(params[:id])
+    if url
+      Rails.logger.info "----------------GETTING FROM CACHE---------------"
+      Rails.logger.info "----------------URL: #{url} ---------------"
+      url = "http://#{url}" unless  url.include?("http")
+      Rails.logger.info "----------------/GETTING FROM CACHE---------------"
+      redirect_to url
+      else
     url_data = Shortener::ShortenedUrl.find_by_unique_key(params[:id])
     if url_data
-      puts '--------------------'
-      puts  url_data.url
-      puts '---------------------'
+      Rails.logger.info '--------From Query-----------'
+      Rails.logger.info  url_data.url
+      Rails.logger.info '---------From Query------------'
       url = "http://#{url_data.url}" unless  url_data.url.include?("http")
       redirect_to url
     end
-  end
+    end
+    end
 end
