cbcf71217e43bcae6f44c985b5552b6735956112
Hamza Zubair <gondal.humza@gmail.com>
2021-10-28 03:53:47 +0200

    refactor and finishing

--- app/controllers/url_compressor_controller.rb
+++ app/controllers/url_compressor_controller.rb
@@ -1,41 +1,52 @@
 class UrlCompressorController < ApplicationController
-  protect_from_forgery with: :null_session
 
   def index
-
   end
+
   def compress_url
     if  params[:url].present? && params[:url].any?
       url_array = []
+      
       params[:url].each do |url|
-      short_url = Shortener::ShortenedUrl.generate(url)
-      Rails.cache.write(short_url.unique_key, url, expires_in: 43200.minutes)
-      short_url.save
-      puts "#{request.inspect}"
-      url_array <<  request.base_url + "/" + short_url.unique_key
+        short_url = Shortener::ShortenedUrl.generate(url)
+        
+        Rails.cache.write(short_url.unique_key, url, expires_in: 43200.minutes)
+        short_url.save
+        
+        url_array <<  request.base_url + "/" + short_url.unique_key
       end
+      
       render json: {status: "ok", data: url_array }
     end
-
-
   end
+
   def redirect_to_base_url
     url = Rails.cache.read(params[:id])
+
     if url
+      url = "http://#{url}" unless  url.include?("http")
+
       Rails.logger.info "----------------GETTING FROM CACHE---------------"
       Rails.logger.info "----------------URL: #{url} ---------------"
-      url = "http://#{url}" unless  url.include?("http")
       Rails.logger.info "----------------/GETTING FROM CACHE---------------"
+
       redirect_to url
-      else
-    url_data = Shortener::ShortenedUrl.find_by_unique_key(params[:id])
-    if url_data
-      Rails.logger.info '--------From Query-----------'
-      Rails.logger.info  url_data.url
-      Rails.logger.info '---------From Query------------'
-      url = "http://#{url_data.url}" unless  url_data.url.include?("http")
-      redirect_to url
+    else
+      url_data = Shortener::ShortenedUrl.find_by_unique_key(params[:id])
+      if url_data
+        url = "http://#{url_data.url}" unless  url_data.url.include?("http")
+
+        Rails.logger.info '--------From Query-----------'
+        Rails.logger.info  url_data.url
+        Rails.logger.info '---------From Query------------'
+
+        redirect_to url
+      end
     end
-    end
-    end
+  end
+
+  private
+  def url_params
+    params.permit(:url)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL ava
-  # ilable within this file, see https://guides.rubyonrails.org/routing.html
-  # resources :url_compressor
 
   root "url_compressor#index"
+
   post  "/compress_url", to: "url_compressor#compress_url"
   get  "/:id", to: "url_compressor#redirect_to_base_url"
 
